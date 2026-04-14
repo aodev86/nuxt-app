df43ec86b4328aa2b78f18f9d141ff3927adea58
aodev86 <aodev86@gmail.com>
2026-04-14 22:57:56 +0900

    step03

---
+++ app/layouts/custom.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>ここがカスタムヘッダー</header>
+        <main>
+            <slot />
+        </main>
+        <footer>ここがカスタムフッター</footer>
+    </div>
+</template>
+<style>
+header {
+    background-color: #e0e0e0;
+    padding: 20px;
+    text-align: center;
+}
+footer {
+    background-color: #e0e0e0;
+    padding: 20px;
+    text-align: center;
+}
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>ここがヘッダー</header>
+        <main>
+            <slot />
+        </main>
+        <footer>ここがフッター</footer>
+    </div>
+</template>
+<style>
+header {
+    background-color: #f0f0f0;
+    padding: 20px;
+    text-align: center;
+}
+footer {
+    background-color: #f0f0f0;
+    padding: 20px;
+    text-align: center;
+}
+</style>
