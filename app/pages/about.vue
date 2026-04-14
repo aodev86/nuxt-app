1f1e3364b1e46a480a546aecadb3ab4e79620f35
aodev86 <aodev86@gmail.com>
2026-04-14 22:48:03 +0900

    step02

---
+++ app/pages/about.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is the about page of our Nuxt application.</p>
+  </div>
+</template>
---
+++ app/pages/posts/[id].vue
@@ -0,0 +1,8 @@
+<script setup>
+const route = useRoute()
+</script>
+
+<template>
+    {{ route.params.id }}番目の投稿だよ
+</template>
+
